1f02dacb24c605ec9d170cbe9b6a566daf2f0121
Daniel Opitz <git@copynpaste.de>
2017-12-12 23:51:47 +0100

    initial

---
+++ bin/build.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+
+const fontManager = require('font-manager'),
+    fontkit = require('fontkit'),
+    fs = require('fs'),
+    path = require('path'),
+    rootDir = path.dirname(__dirname),
+    filesDir = path.join(rootDir, 'files');
+
+fontManager.getAvailableFonts(function (fonts) {
+    fonts.forEach(function (fontData) {
+        var fileName = [
+            fontData.family,
+            fontData.weight,
+            fontData.width,
+            +fontData.italic,
+            fontData.style
+        ].join('_').replace(' ', '-').toLowerCase() + '.json';
+
+        fontkit.open(fontData.path, function (err, font) {
+            if (!err && font.characterSet) {
+                var widths = {};
+
+                font.characterSet.forEach(function (value) {
+                    widths[value] = font.glyphForCodePoint(value).advanceWidth / font.unitsPerEm;
+                });
+
+                fs.writeFile(path.join(filesDir, fileName), JSON.stringify(widths), function (err) {
+                    if (err) {
+                        console.error(err);
+                    }
+                });
+            }
+        });
+    });
+});
